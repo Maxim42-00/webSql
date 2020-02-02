b31c5af58cbbf5fd870559c3033ddbab3aac3b56
Maxim42-00 <alfred.hammersmit@yandex.ru>
2020-02-02 14:47:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+﻿"use strict"
+
+let db;
+
+let goodCreate = `
+    create table good(
+        id integer primary key autoincrement,
+        title varchar(255),
+        description varchar(1024)
+    );
+`;
+
+let goodInsert = `insert into good(title, description) values(?, ?)`;
+
+try
+{
+    db = openDatabase('huw5', '1.0', 'Test DB', 2*1024*1024);
+}
+catch
+{
+    alert("У тебя в браузере нет WebSql. =(");
+}
+
+function initGood()
+{
+    db.transaction(function(tx){
+        tx.executeSql(goodCreate, 
+        	          [], 
+        	          ()=>{console.log('good table has been created saccessfully')},
+        	          ()=>{console.log('good table hasn\'t been created saccessfully')});
+
+        tx.executeSql(goodInsert, 
+        	          ['велосипед', 'полезен для здоровья'], 
+        	          ()=>{console.log('data has been inserted saccessfully')},
+        	          ()=>{console.log('data hasn\'t been inserted saccessfully')});
+    });
+}
+
+function success(tx, result)
+{
+    console.log(result.rows);
+}
+
+function error(tx, error)
+{
+    console.log(error.message);
+}
+
+let callback = function(tx)
+{
+    tx.executeSql(`select 'привет' as world`, [], success, error);
+    tx.executeSql(`select_ 'привет' as world`, [], success, error);
+}
+
+db.transaction(callback);
